f4abb8200e49024311d9fd8f5130ed189e139fe3
Andrew Lee <alee14498@protonmail.com>
2023-07-15 14:00:33 -0400

    Added title for updates page

--- app/updates/page.js
+++ app/updates/page.js
@@ -1,6 +1,11 @@
 import getPostMetadata from "@/components/getPostMetadata";
 import PostPreview from "@/components/PostPreview";
 
+export const metadata = {
+    title: 'Updates',
+    description: 'Follow the latest updates from the Government of Alure Regions',
+}
+
 export default function Updates() {
     const postMetadata = getPostMetadata();
     const postPreviews = postMetadata.map((post) => (
@@ -10,8 +15,8 @@ export default function Updates() {
         <main className="flex flex-col">
             <div className="bg-center bg-no-repeat bg-[url('/jumbotron.webp')] bg-gray-700 bg-blend-multiply">
                 <div className="md:px-40 px-10 max-w-screen-xl text-left py-24 lg:py-10 space-y-3">
-                    <h1 className="font-medium text-4xl">Updates</h1>
-                    <h2 className="font-light text-lg">Follow the latest updates from the Government of Alure Regions</h2>
+                    <h1 className="font-medium text-4xl">{metadata.title}</h1>
+                    <h2 className="font-light text-lg">{metadata.description}</h2>
                 </div>
             </div>
             <div className="md:px-60 px-2 py-3">
