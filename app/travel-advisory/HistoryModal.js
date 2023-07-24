dd8050e18db6db2fad0d8c59d5e2ef0c11fe155f
Andrew Lee <alee14498@protonmail.com>
2023-07-23 20:05:08 -0400

    Updated travel advisory

--- app/travel-advisory/HistoryModal.js
+++ app/travel-advisory/HistoryModal.js
@@ -12,7 +12,7 @@ const HistoryModal = ({ isVisible, onClose, countries, dangerLevel, history }) =
             )
         })
     } else {
-        historyList = <li>There is currently a parasite going around the world, travel carefully.</li>;
+        historyList = <li>Currently no diplomatic/medical issues in this country.</li>;
     }
 
     return (
